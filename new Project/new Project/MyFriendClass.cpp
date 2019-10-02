e2a07561b856164d49b81f2425b5d2e0800718b5
kkwoo0620 <52120400+kkwoo0620@users.noreply.github.com>
2019-10-02 22:13:49 +0900

    friend
    friend 선언하면 선언한 객체에게 private 접근 가능하게 함.
    전역함수, 멤버함수도 friend 가능

---
+++ new Project/new Project/MyFriendClass.cpp
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<cstring>
+
+using std::cout;
+using std::endl;
+
+class Girl;
+
+class Boy
+{
+private:
+	int height;
+	friend class Girl;
+public:
+	Boy(int len) :height(len) {}
+	void ShowYourFriendInfo(Girl &frn);
+};
+
+class Girl
+{
+private:
+	char phNum[20];
+
+public:
+	Girl(const char * num)
+	{
+		strcpy(phNum, num);
+	}
+	void ShowYourFriendInfo(Boy &frn);
+	friend class Boy;
+};
+
+void Boy::ShowYourFriendInfo(Girl &frn) {
+	cout << "Her phone number: " << frn.phNum << endl;
+}
+
+void Girl::ShowYourFriendInfo(Boy &frn) {
+	cout << "His height: " << frn.height << endl;
+}
+
+
+int main() {
+
+}
